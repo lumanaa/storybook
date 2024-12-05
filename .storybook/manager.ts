f9beca3488c4ef3f06305a5994990e6178fbf6b9
lumanaa <lssuwal@gmail.com>
2024-12-05 14:54:28 +0545

    theme change

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,6 +4,7 @@ import { create } from '@storybook/theming';
 addons.setConfig({
     theme: create({
         base: "dark",
+        brandTitle: "Bring ideas to life",
         brandUrl: "https://lumana.dev",
         brandImage: "/storybook.png",
         brandTarget: '_self'
