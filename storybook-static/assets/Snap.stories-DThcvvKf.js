133c02a41fa458d5ecbc004290257335cde482f8
lumanaa <lssuwal@gmail.com>
2024-12-05 14:39:14 +0545

    build

---
+++ storybook-static/assets/Snap.stories-DThcvvKf.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import{r as o}from"./index-DJO9vBfz.js";/* empty css             */const a=({children:m})=>(o.useEffect(()=>{const l=()=>{const i=window.innerHeight*100,d=(document.documentElement.scrollTop||document.body.scrollTop)/i;d<=.005?(document.getElementById("dd1").className="",document.getElementById("dd2").className="",document.getElementById("dd3").className="",document.getElementById("dd4").className="",document.getElementById("dd5").className="",document.getElementById("dd6").className=""):d>.005&&d<=.015?(document.getElementById("dd1").className="active22",document.getElementById("dd2").className="active22",document.getElementById("dd3").className="active22",document.getElementById("dd4").className="active22",document.getElementById("dd5").className="active22",document.getElementById("dd6").className="active22"):d>.015&&d<=.025?(document.getElementById("dd1").className="active23",document.getElementById("dd2").className="active23",document.getElementById("dd3").className="active23",document.getElementById("dd4").className="active23",document.getElementById("dd5").className="active23",document.getElementById("dd6").className="active23"):d>.025&&d<=.035?(document.getElementById("dd1").className="active24",document.getElementById("dd2").className="active24",document.getElementById("dd3").className="active24",document.getElementById("dd4").className="active24",document.getElementById("dd5").className="active24",document.getElementById("dd6").className="active24"):d>.035&&d<=.045?(document.getElementById("dd1").className="active25",document.getElementById("dd2").className="active25",document.getElementById("dd3").className="active25",document.getElementById("dd4").className="active25",document.getElementById("dd5").className="active25",document.getElementById("dd6").className="active25"):(document.getElementById("dd1").className="active26",document.getElementById("dd2").className="active26",document.getElementById("dd3").className="active26",document.getElementById("dd4").className="active26",document.getElementById("dd5").className="active26",document.getElementById("dd6").className="active26")};return window.onscroll=l,()=>{window.onscroll=null}},[]),e.jsx("div",{className:"snap-body",children:e.jsx("div",{className:"snap-container",children:m})}));a.__docgenInfo={description:"",methods:[],displayName:"SnapScroll",props:{children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const v={component:a,tags:["autodocs"],parameters:{layout:"fullscreen"}},s={args:{},render:()=>e.jsxs(a,{children:[e.jsx("div",{id:"dd1",className:"dd",children:e.jsx("span",{className:"sp1",children:"View on full screen"})}),e.jsx("div",{id:"dd2",className:"dd",children:e.jsx("span",{className:"sp1",children:"2"})}),e.jsx("div",{id:"dd3",className:"dd",children:e.jsx("span",{className:"sp1",children:"3"})}),e.jsx("div",{id:"dd4",className:"dd",children:e.jsx("span",{className:"sp1",children:"4"})}),e.jsx("div",{id:"dd5",className:"dd",children:e.jsx("span",{className:"sp1",children:"5"})}),e.jsx("div",{id:"dd6",className:"dd",children:e.jsx("span",{className:"sp1",children:"6"})})]})};var t,n,c;s.parameters={...s.parameters,docs:{...(t=s.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    return <SnapScrollComponent>\r
+        <div id='dd1' className='dd'>\r
+          <span className='sp1'>View on full screen</span>\r
+        </div>\r
+        <div id='dd2' className='dd'>\r
+          <span className='sp1'>2</span>\r
+        </div>\r
+        <div id='dd3' className='dd'>\r
+          <span className='sp1'>3</span>\r
+        </div>\r
+        <div id='dd4' className='dd'>\r
+          <span className='sp1'>4</span>\r
+        </div>\r
+        <div id='dd5' className='dd'>\r
+          <span className='sp1'>5</span>\r
+        </div>\r
+        <div id='dd6' className='dd'>\r
+          <span className='sp1'>6</span>\r
+        </div>\r
+      </SnapScrollComponent>;
+  }
+}`,...(c=(n=s.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const g=["SnapScroll"];export{s as SnapScroll,g as __namedExportsOrder,v as default};
